b854b3c86f5a688e87332c350d8a3a8d964822e4
andrew <slayer2004@list.ru>
2026-02-04 14:24:04 +0300

    f

---
+++ docx-master/jszip-main/rollup.config.mjs
@@ -0,0 +1,25 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+
+export default {
+  input: "lib/jszip-entry.js",
+  output: [
+    {
+      file: "dist/index.js",
+      format: "es",
+      sourcemap: true,
+    },
+    // {
+    //   file: "dist/jszip.cjs.js",
+    //   format: "cjs",
+    //   exports: "default",
+    //   sourcemap: true,
+    // }
+  ],
+  plugins: [
+    resolve({ browser: true, preferBuiltins: false }),
+    commonjs({
+      transformMixedEsModules: true,
+    }),
+  ],
+};
